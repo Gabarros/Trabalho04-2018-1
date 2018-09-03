3e21d6432c5c85a76c1b1f5eb9ad21475ce88bbf
Gabarros <gabarros17@gmail.com>
2018-09-03 11:24:38 -0300

    Created AutoCompleteTextView

--- app/src/main/java/com/example/gabriel/trabalho04_2018_1/Consulta.java
+++ app/src/main/java/com/example/gabriel/trabalho04_2018_1/Consulta.java
@@ -2,6 +2,8 @@ package com.example.gabriel.trabalho04_2018_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -11,7 +13,7 @@ import android.widget.TextView;
 public class Consulta extends AppCompatActivity {
 
     Button btConsulta, btLimpar;
-    EditText etMarca, etAno, etModelo;
+    AutoCompleteTextView etMarca, etAno, etModelo;
     TextView tvValor;
     RadioGroup rg;
     RadioButton rbCarro, rbMoto, rbCaminha;
@@ -25,11 +27,41 @@ public class Consulta extends AppCompatActivity {
         btLimpar = findViewById(R.id.btLimpar);
 
         etMarca = findViewById(R.id.etMarca);
-        etAno = findViewById(R.id.etAno);
+        etAno = findViewById(R.id.etModelo);
         etModelo = findViewById(R.id.etModelo);
 
         rg = findViewById(R.id.radioGroup);
 
         tvValor = findViewById(R.id.tvValor);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, MARCAS);
+        AutoCompleteTextView textView = (AutoCompleteTextView)
+                findViewById(R.id.etMarca);
+        textView.setAdapter(adapter);
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, MODELOS);
+        AutoCompleteTextView textView2 = (AutoCompleteTextView)
+                findViewById(R.id.etModelo);
+        textView.setAdapter(adapter);
+
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, ANOS);
+        AutoCompleteTextView textView3 = (AutoCompleteTextView)
+                findViewById(R.id.etAno);
+        textView.setAdapter(adapter);
     }
+
+    private static final String[] MARCAS = new String[] {
+            "Belgium", "France", "Italy", "Germany", "Spain"
+    };
+
+    private static final String[] MODELOS = new String[] {
+            "Belgium", "France", "Italy", "Germany", "Spain"
+    };
+
+    private static final String[] ANOS = new String[] {
+            "Belgium", "France", "Italy", "Germany", "Spain"
+    };
 }
--- app/src/main/java/com/example/gabriel/trabalho04_2018_1/MainActivity.java
+++ app/src/main/java/com/example/gabriel/trabalho04_2018_1/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+        AutoCompleteTextView textView = (AutoCompleteTextView)
+                findViewById(R.id.list);
+        textView.setAdapter(adapter);
 
 
         Button btInicial = findViewById(R.id.btInicial);
@@ -29,4 +37,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private static final String[] COUNTRIES = new String[] {
+            "Belgium", "France", "Italy", "Germany", "Spain"
+    };
 }
